0c8fab611632fe88fba062532002c1a6a75c18db
Tatana <burmatova@inbox.ru>
2021-04-07 18:34:34 +0300

    added MoneyTransferTests.java

---
+++ src/test/java/ru/netology/web/test/MoneyTransferTests.java
@@ -0,0 +1,40 @@
+package ru.netology.web.test;
+
+import lombok.val;
+import org.junit.jupiter.api.Test;
+import ru.netology.web.data.DataHelper;
+import ru.netology.web.page.DashboardPageOfCardsRecharge;
+import ru.netology.web.page.LoginPage;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class MoneyTransferTests {
+
+    @Test
+    void shouldTransferMoneyFromFirstCardToTheSecond() {
+        open("http://localhost:9999");
+        val loginPage = new LoginPage();
+        val authInfo = DataHelper.getAuthInfo();
+        val verificationPage = loginPage.validLogin(authInfo);
+        val verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+        verificationPage.validVerify(verificationCode);
+        val dashboardPageOfCardsRecharge = new DashboardPageOfCardsRecharge();
+        dashboardPageOfCardsRecharge.transferMoneyFromFirstCardToTheSecond
+                ("200", DataHelper.getSecondCardNumber());
+    }
+
+    @Test
+    void shouldTransferMoneyFromSecondCardToTheFirst() {
+        open("http://localhost:9999");
+        val loginPage = new LoginPage();
+        val authInfo = DataHelper.getAuthInfo();
+        val verificationPage = loginPage.validLogin(authInfo);
+        val verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+        verificationPage.validVerify(verificationCode);
+        val dashboardPageOfCardsRecharge = new DashboardPageOfCardsRecharge();
+        dashboardPageOfCardsRecharge.transferMoneyFromSecondCardToTheFirst
+                ("200", DataHelper.getSecondCardNumber());
+
+    }
+
+}
