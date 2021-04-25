1cd91d04dfcf170313552670f21d90988e101914
Tatana <burmatova@inbox.ru>
2021-04-25 18:01:03 +0300

    added method returnCardsBalancesToInitialAmounts

--- src/test/java/ru/netology/web/page/DashboardPageOfCardsRecharge.java
+++ src/test/java/ru/netology/web/page/DashboardPageOfCardsRecharge.java
@@ -3,7 +3,7 @@ package ru.netology.web.page;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
-import lombok.val;
+import org.openqa.selenium.Keys;
 import ru.netology.web.data.DataHelper;
 
 import static com.codeborne.selenide.Selectors.withText;
@@ -18,36 +18,24 @@ public class DashboardPageOfCardsRecharge {
     private SelenideElement buttonActionTransfer = $("[data-test-id=action-transfer]");
 
         public DashboardPageOfCards transferMoneyFromFirstCardToTheSecond
-                (DataHelper.AmountForTransfer amountForTransfer, DataHelper.CardNumber cardNumber) {
+                (String amountForTransfer, DataHelper.CardNumber cardNumber) {
         buttonsRefill.last().click();
         heading.shouldBe(Condition.visible);
-        fieldAmount.setValue(amountForTransfer.getAmount());
+        fieldAmount.setValue(amountForTransfer);
         fieldFrom.setValue(cardNumber.getNumber());
         buttonActionTransfer.click();
 
         return new DashboardPageOfCards();
     }
 
-    public DashboardPageOfCards transferMoneyFromSecondCardToTheFirst
-            (DataHelper.AmountForTransfer amountForTransfer, DataHelper.CardNumber cardNumber) {
+    public DashboardPageOfCards returnCardsBalancesToInitialAmounts
+            (DataHelper.CardNumber cardNumber) {
         buttonsRefill.first().click();
         heading.shouldBe(Condition.visible);
-        fieldAmount.setValue(amountForTransfer.getAmount());
+        fieldFrom.sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
         fieldFrom.setValue(cardNumber.getNumber());
         buttonActionTransfer.click();
 
         return new DashboardPageOfCards();
     }
-
-    public DashboardPageOfCards notTransferMoneyFromFirstCardToTheSecond
-            (DataHelper.AmountForTransfer amountForTransfer, DataHelper.CardNumber cardNumber) {
-        buttonsRefill.last().click();
-        heading.shouldBe(Condition.visible);
-        fieldAmount.setValue(amountForTransfer.getAmount());
-        fieldFrom.setValue(cardNumber.getNumber());
-        buttonActionTransfer.click();
-
-        return new DashboardPageOfCards();
-    }
-
 }
