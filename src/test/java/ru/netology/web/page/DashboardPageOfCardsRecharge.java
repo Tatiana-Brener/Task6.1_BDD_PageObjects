dda8b3f4d8fea76feab32653ef1734b1c70429ef
Tatana <burmatova@inbox.ru>
2021-04-29 19:49:49 +0300

    fixed method transferMoney

--- src/test/java/ru/netology/web/page/DashboardPageOfCardsRecharge.java
+++ src/test/java/ru/netology/web/page/DashboardPageOfCardsRecharge.java
@@ -8,18 +8,16 @@ import ru.netology.web.data.DataHelper;
 
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$$;
 
 public class DashboardPageOfCardsRecharge {
-    private ElementsCollection buttonsRefill= $$("[data-test-id=action-deposit]");
     private SelenideElement heading = $(withText("Пополнение карты"));
     private SelenideElement fieldAmount = $("[data-test-id=amount] .input__control");
     private SelenideElement fieldFrom = $("[data-test-id=from] .input__control");
     private SelenideElement buttonActionTransfer = $("[data-test-id=action-transfer]");
 
-        public DashboardPageOfCards transferMoneyFromFirstCardToTheSecond
-                (String amountForTransfer, DataHelper.CardNumber cardNumber) {
-        buttonsRefill.last().click();
+    public DashboardPageOfCards transferMoney (ElementsCollection buttonForTransfer,
+                                               String amountForTransfer,
+                                               DataHelper.CardNumber cardNumber) {
         heading.shouldBe(Condition.visible);
         fieldAmount.setValue(amountForTransfer);
         fieldFrom.setValue(cardNumber.getNumber());
@@ -29,8 +27,7 @@ public class DashboardPageOfCardsRecharge {
     }
 
     public DashboardPageOfCards returnCardsBalancesToInitialAmounts
-            (DataHelper.CardNumber cardNumber) {
-        buttonsRefill.first().click();
+            (ElementsCollection buttonForTransfer, DataHelper.CardNumber cardNumber) {
         heading.shouldBe(Condition.visible);
         fieldFrom.sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
         fieldFrom.setValue(cardNumber.getNumber());
